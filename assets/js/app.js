7fbca51fe10fd9bfd044850a27601b180c778750
Giulio <62805702+knightrider909@users.noreply.github.com>
2020-07-09 18:10:54 +0100

    changes
    new changes made, footer created

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,52 @@
+//produces an alert for the userAgent, The userAgent property returns the value of the user-agent header sent
+//by the browser to the server. The value returned, contains information about the name, version and platform
+//of the browser.
 
+function userAgent() {
+  console.log("before the function");
 
-function userAgent(){
-    console.log("before the function");
-    setTimeout(function(){
-        alert(window.navigator.userAgent);
-        console.log("inside the function");
-    },2000);
+  setTimeout(function () {
+    alert(window.navigator.userAgent);
+    console.log("inside the function");
+  }, 2000);
 }
 
 //userAgent();
+
+// this provides the json object information
+
+function getUserInfo() {
+  $.getJSON("http://127.0.0.1:5500/assets/js/info.json", function (data) {
+    console.log(data);
+    alert(data.geobytescapital);
+  });
+}
+
+//getUserInfo();
+
+// using the below you can produce specific infomraiton, here the title of the web page is changed to
+// show the location of the user aswell
+
+function changeTitle() {
+  $.getJSON("http://127.0.0.1:5500/assets/js/info.json", function (data) {
+    jQuery(".navbar-brand").html(
+      jQuery(".navbar-brand").html() + " (" + data.geobytescapital + ") "
+    );
+  });
+}
+
+//changeTitle();
+
+// after a 3 second wait the user country locaton is shown in the footer of the web page
+
+function userLocation() {
+  setTimeout(localCountry, 3000);
+}
+
+userLocation();
+
+function localCountry() {
+  $.getJSON("http://127.0.0.1:5500/assets/js/info.json", function (data) {
+    jQuery(".country-location").html(data.geobytescountry);
+  });
+}
