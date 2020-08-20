d69514764dbd92d1b1ac0e58acf0237c034aba61
Giulio <62805702+knightrider909@users.noreply.github.com>
2020-08-20 17:03:35 +0100

    changes

--- termsandconditions.php
+++ termsandconditions.php
@@ -1,5 +1,3 @@
-
-
 <div class="bannertop"></div>
 
 <div class="content">
@@ -20,23 +18,23 @@
       1.2 We operate the website www.hellofresh.co.uk (the “Website”).
     </li>
     <li>
-      1.3 This document (together with the documents referred to in it) tells
-      you the terms and conditions on which we will supply to you the products
-      (“Products”) listed on our Site via one of our subscription services
-      (“Services”). Please read these terms and conditions carefully before
-      ordering any Products from our site or subscribing to one of our Services.
-      You should understand that by ordering any of our Products or subscribing
-      to one of our Services, you agree to be bound by these terms and
-      conditions.
+      1.3 This document (together with the documents referred to in it)
+      tells you the terms and conditions on which we will supply to you the
+      products (“Products”) listed on our Site via one of our subscription
+      services (“Services”). Please read these terms and conditions
+      carefully before ordering any Products from our site or subscribing to
+      one of our Services. You should understand that by ordering any of our
+      Products or subscribing to one of our Services, you agree to be bound
+      by these terms and conditions.
     </li>
     <li>
-      1.4 These Terms and Conditions were most recently updated on 6 April 2020
-      and apply to sales to consumers.
+      1.4 These Terms and Conditions were most recently updated on 6 April
+      2020 and apply to sales to consumers.
     </li>
     <li>
-      1.5 If you use or order Products after we have published any changes you
-      will be bound by those changes. You should check prior to each use or
-      order to ensure that you understand the precise terms & conditions
+      1.5 If you use or order Products after we have published any changes
+      you will be bound by those changes. You should check prior to each use
+      or order to ensure that you understand the precise terms & conditions
       applicable to your visit to our Website, or purchase.
     </li>
     <li>
@@ -44,10 +42,10 @@
       reference.
     </li>
     <li>
-      1.7 Please tick the checkbox in the shopping basket to accept these terms
-      and conditions. Please understand that if you refuse to accept these terms
-      and conditions, you will not be able to order any Products from our
-      website.
+      1.7 Please tick the checkbox in the shopping basket to accept these
+      terms and conditions. Please understand that if you refuse to accept
+      these terms and conditions, you will not be able to order any Products
+      from our website.
     </li>
   </ul>
 
@@ -58,11 +56,12 @@
   <div class="line"></div>
 
   <p class="section2text setin">
-    2.1 Our website is only intended for use by people residing in the United
-    Kingdom of Great Britain and Northern Ireland, and the Channel Islands (as
-    applicable) (“Serviced Countries”). Unfortunately, we cannot accept orders
-    from individuals outside of these Services Countries at this time, or from
-    certain postcodes in the Scottish Highlands and Islands.
+    2.1 Our website is only intended for use by people residing in the
+    United Kingdom of Great Britain and Northern Ireland, and the Channel
+    Islands (as applicable) (“Serviced Countries”). Unfortunately, we cannot
+    accept orders from individuals outside of these Services Countries at
+    this time, or from certain postcodes in the Scottish Highlands and
+    Islands.
   </p>
 
   <div class="line"></div>
