6b99107ae819b74ab69796e47d0700229971652a
Giulio <62805702+knightrider909@users.noreply.github.com>
2020-08-29 11:21:06 +0100

    changes

--- assets/js/carTable.js
+++ assets/js/carTable.js
@@ -1,3 +1,54 @@
+// jquery append example
+
+$(document).ready(function () {
+  $("#inputButton").click(function () {
+    // make = $("#make").val();
+    // model = $("#model").val();
+    // price = $("#price").val();
+    //console.log(make + " " + model + " " + price);
+    $("tbody").append(
+      "<tr><td>" +
+        $("#make").val() +
+        "</td> + <td> " +
+        $("#model").val() +
+        " </td> +  <td>" +
+        $("#price").val() +
+        " </td></tr> "
+    );
+  });
+});
+
+// function tester1() {
+//   alert("worrking exampler");
+// }
+
+// tester1();
+
+$(document).ready(function () {
+  $("#buttonTest").click(function () {
+    $("p").hide();
+  });
+});
+
+function lessThan(input1, input2) {
+  inputTotal = input1 + input2;
+
+  if (isNaN(inputTotal)) {
+    return "please enter a number";
+  } else if (inputTotal < 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(lessThan(29, 70));
+
+//table code
+$(document).ready(function () {
+  $("#table_Jquery").DataTable();
+});
+
 // alert("car");
 
 // function tableTest() {
@@ -17,38 +68,3 @@
 // }
 
 // game();
-
-$(document).ready(function () {
-  $("#inputButton").click(function () {
-    make = $("#make").val();
-    model = $("#model").val();
-    price = $("#price").val();
-    console.log(make + " " + model + " " + price);
-  });
-});
-
-// function tester1() {
-//   alert("worrking exampler");
-// }
-
-// tester1();
-
-$(document).ready(function () {
-  $("#buttonTest").click(function () {
-    $("p").hide();
-  });
-});
-
-function lessThan(input1, input2) {
-  inputTotal = input1 + input2;
-
-  if (typeof inputTotal == "undefined") {
-    return "please enter a number";
-  } else if (inputTotal < 100) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log(lessThan());
