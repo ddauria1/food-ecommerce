a1b713acaa58fcb43f334188731b1252d5f305eb
webdevelopment@interregs.com <HbrgFvrtayn37561hsgwcFnH>
2020-09-10 18:38:16 +0100

    Added connection to DB

---
+++ lib/config.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new PDO("mysql:host=$servername;dbname=ecommerce", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+function getPageContent($pageName){
+    global $conn;
+
+    $stmt = $conn->prepare("SELECT * FROM pages WHERE NAME='".$pageName."'");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $stmt->fetchAll();
+    $result = $result[0];
+
+    return $result;
+
+}
